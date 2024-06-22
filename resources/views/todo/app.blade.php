fae0c0e0b6fff11e9585e6a851b459693b2dc67d
Raiff Hakan <raiipp04@gmail.com>
2024-06-22 22:55:29 +0700

    first commit

--- resources/views/todo/app.blade.php
+++ resources/views/todo/app.blade.php
@@ -9,29 +9,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-light">
+<body class=""
     <!-- 00. Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid col-md-7">
-            <div class="navbar-brand">Simple To Do List</div>
-            <!--
-            <div class="navbar-collapse justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Akun Saya
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Logout</a></li>
-                            <li><a class="dropdown-item" href="#">Update Data</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        -->
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center">
+            <div class="navbar-brand">WEB APPLICATION SEDERHANA UNTUK MENGELOLA KEGIATAN HARIAN</div>
     </nav>
+
 
     <div class="container mt-4">
         <!-- 01. Content-->
@@ -143,7 +126,7 @@
     </div>
 
     <!-- Bootstrap JS Bundle (popper.js included) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>>
 
 </body>
 
